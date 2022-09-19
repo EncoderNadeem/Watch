bf5a172bf1e60e9aa4e870864e4b71bbf113a277
EncoderNadeem <110691875+EncoderNadeem@users.noreply.github.com>
2022-09-20 01:21:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+setInterval(()=>{
+
+    date=new Date();
+    Htime =date.getHours();
+    Mtime=date.getMinutes();
+    Stime=date.getSeconds();
+    Hrotation= 30*Htime + Mtime/2; 
+
+    Mrotation= 6*Mtime;
+
+    Srotation=6*Stime; 
+
+    hour.style.transform=`rotate(${Hrotation}deg)`;  
+    minute.style.transform=`rotate(${Mrotation}deg)`;  
+    second.style.transform=`rotate(${Srotation}deg)`;  
+
+
+},1000)
